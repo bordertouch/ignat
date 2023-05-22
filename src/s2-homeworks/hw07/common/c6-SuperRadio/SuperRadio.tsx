5fff7dc3f5f8bdfcc391783f1347bb24a87a2031
bordertouch <bordertouch@gmail.com>
2023-05-22 13:39:19 +0300

    'some_details'

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -35,7 +35,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ...restProps
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        debugger
+
         onChangeOption && onChangeOption(e.currentTarget.value)
 
         onChange?.(e)
